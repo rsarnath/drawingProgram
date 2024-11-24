188dd80d5dc04f8793a5b555820d95c896088758
rsarnath <136544306+rsarnath@users.noreply.github.com>
2024-11-24 16:51:22 -0600

    Delete Samplecode directory

--- Samplecode/DrawingProgram/UIContext.java
+++
@@ -1,6 +0,0 @@
-import java.awt.*;
-public interface UIContext {
-  //  public abstract void drawCircle(Circle circle);
-  public abstract void drawLine(Point point1, Point point2 );
-  public abstract void drawLabel(String s, Point p);
-}
