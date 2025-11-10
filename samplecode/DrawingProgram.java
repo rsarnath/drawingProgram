abcf0abe4043bfdc6edde0f3bf5af9dc7ded799f
rsarnath <136544306+rsarnath@users.noreply.github.com>
2025-11-10 23:07:49 +0000

    2025NOV10PM5

--- samplecode/DrawingProgram.java
+++ samplecode/DrawingProgram.java
@@ -1,5 +1,7 @@
+import javax.swing.*;
 public class DrawingProgram {
   public static void main(String[] args){
+    JFrame.setDefaultLookAndFeelDecorated(true);
     Model model = new Model();
     UndoManager undoManager = new UndoManager();
     UndoManager.setModel(model);
